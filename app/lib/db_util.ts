c73386b3676e5fa38f339098b992e0fc4b027d14
Jacob Cell <59305245+Thirstymidget28@users.noreply.github.com>
2024-11-13 14:44:42 -0600

    Update db_util.ts

--- app/lib/db_util.ts
+++ app/lib/db_util.ts
@@ -8,9 +8,10 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
-    cert: process.env.DB_CLIENT_CERT,
-    key: process.env.DB_CLIENT_KEY,
-    ca: process.env.DB_SERVER_CA
+    rejectUnauthorized: true,
+    ca: process.env.DB_SERVER_CA ? process.env.DB_SERVER_CA.replace(/\\n/g, '\n') : undefined,
+    key: process.env.DB_CLIENT_KEY ? process.env.DB_CLIENT_KEY.replace(/\\n/g, '\n') : undefined,
+    cert: process.env.DB_CLIENT_CERT ? process.env.DB_CLIENT_CERT.replace(/\\n/g, '\n') : undefined
   },
   waitForConnections: true,   // Wait for available connection if the pool is full
   connectionLimit: 10,        // Max number of simultaneous connections
